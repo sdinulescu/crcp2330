09c9ecffd7f5595dbbbef78c24bac83a328b03c3
sdinulescu <stejara@gmail.com>
2018-03-04 23:20:44 -0600

    Mult Completed on CPUEmulator

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,32 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
+
+//RAM[24576] is where key pressed on the physical keyboard
+
 // Put your code here.
+
+
+
+
+(LOOP)
+	@KBD
+	D=M 			//grabs whether or not a key is pressed
+	@BLACK
+	D;JNE 			//go to black if out doesn't equal 0 (if key is pressed)
+
+(WHITE)
+	@SCREEN
+	M=0 			//fill with white
+
+
+(BLACK)
+	@SCREEN
+	M=-1 			//fill with black
+	@END
+	0;JMP
+
+
+(END)
+	@LOOP
+	0;JMP 			//infinite loop
